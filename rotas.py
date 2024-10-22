4044d8d2f664bc039356ef12d06732722c3bf3e2
anderson1904 <andersoncosta190406@gmail.com>
2024-10-22 14:24:30 -0300

    criando o esquema de rotas

--- rotas.py
+++ rotas.py
@@ -2,7 +2,25 @@ import flet as ft
 from views import(
     cadastro,login
 )
-def rota_cadastro():
-    pass
-def rota_login():
-    pass
+
+def rota_login(page: ft.Page):
+    return ft.View(
+        '/',
+        controls=[
+            login.cabeçalho(),
+            login.login(),
+            ft.Text('não está cadastrado?',size=25),
+            ft.ElevatedButton('cadastre-se aqui', on_click=lambda _: page.go("/cadastro"))
+        ]
+    )
+
+def rota_cadastro(page:ft.Page):
+    return ft.View(
+        '/cadastro',
+        controls=[
+            cadastro.cabeçalho(),
+            cadastro.cadastro(),
+            ft.Text('já possui conta?', size=25),
+            ft.ElevatedButton(' faça seu login', on_click=lambda _: page.go("/"))
+        ]
+    )
