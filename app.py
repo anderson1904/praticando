1ea246f33edfc81413684aa5487a02e6586dc1d7
anderson1904 <andersoncosta190406@gmail.com>
2024-10-22 14:28:35 -0300

    criando a paginação e o tamanho da aplicação flet

--- app.py
+++ app.py
@@ -5,7 +5,20 @@ from rotas import(
 )
 
 def main(page: ft.Page):
-    def paginação():
-        pass
+    page.window_width=300
+    page.window_height=400
+    page.update()
+
+    def paginação(route):
+        page.views.clear()
+        if page.route == "/":
+            page.views.append(rota_login(page))
+        elif page.route == "/cadastro":
+            page.views.append(rota_cadastro(page))
+    page.update()
+    page.on_route_change = paginação
+    page.go(page.route)
+
+
     
 ft.app(target=main)
